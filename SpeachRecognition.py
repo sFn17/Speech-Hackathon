de861bb70f3cfcfa543f933a3b9504aeae02e840
Rares <rares.braga@eestec.ro>
2023-05-31 19:05:40 +0300

    test

--- SpeachRecognition.py
+++ SpeachRecognition.py
@@ -25,7 +25,7 @@ def get_answer(question, file_path):
     return "Nu am găsit un răspuns pentru această întrebare."
 
 
-excel_file_path = 'C:\\Users\\rares\\Desktop\\Speech\\rasp.xlsx'
+excel_file_path = 'C:\\Users\\rares\\Desktop\\hackathon\\speech\\rasp.xlsx'
 
 
 wb = openpyxl.load_workbook(excel_file_path)
